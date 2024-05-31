8b24c2f1dce202cb355cf7a795ae797e0cba161e
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-05-30 18:53:26 -0600

    retoques

--- footer/footer.js
+++ footer/footer.js
@@ -5,9 +5,9 @@ footer_sof.innerHTML =`
 <img src="https://media.licdn.com/dms/image/C560BAQFReQup08LukQ/company-logo_200_200/0/1652286229007?e=2147483647&v=beta&t=cB65tfGD72TrPKNWt3aSnaNNPKKTzlrgGY4vTAMhpyg" alt="">
 </div>
 <div class ="github">
-<a href="">Ariel</a>
-<a href="">Génesis</a>
-<a href="">Kennet</a>
-<a href="">Sofía</a>
+<a href="https://github.com/Leibril2007/">Ariel</a>
+<a href="https://github.com/marianaseven2013/">Génesis</a>
+<a href="https://github.com/keneth778/">Kennet</a>
+<a href="https://github.com/sofiagarcia358/">Sofía</a>
 </div>
 `;
