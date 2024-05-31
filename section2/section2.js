0ed5ea8975b9418ad00ec72cddc52a3f9a6334e2
keneth <keneth.javier90@gmail.com>
2024-05-30 18:24:32 -0600

    section2_keneth

--- section2/section2.js
+++ section2/section2.js
@@ -1,5 +1,59 @@
 let section2_sof =document.querySelector(".section-2");
 
 section2_sof.innerHTML =`
-soy section2
+<table>
+<tr>
+    <th>Etiqueta HTML</th>
+    <th>Descripción</th>
+</tr>
+<tr>
+    <td>&lt;div&gt;</td>
+    <td>Define una sección</td>
+</tr>
+<tr>
+    <td>&lt;p&gt;</td>
+    <td>Define un párrafo de texto.</td>
+</tr>
+<tr>
+    <td>&lt;span&gt;</td>
+    <td>Define un contenedor en línea para el contenido.</td>
+</tr>
+<tr>
+    <td>&lt;img&gt;</td>
+    <td>Define una imagen.</td>
+</tr>
+<tr>
+    <td>&lt;a&gt;</td>
+    <td>Define un hipervínculo.</td>
+</tr>
+<tr>
+    <td>&lt;ul&gt;</td>
+    <td>Define una lista desordenada.</td>
+</tr>
+<tr>
+    <td>&lt;ol&gt;</td>
+    <td>Define una lista ordenada.</td>
+</tr>
+<tr>
+    <td>&lt;li&gt;</td>
+    <td>Define un elemento de lista en ul u ol.</td>
+</tr>
+<tr>
+    <td>&lt;table&gt;</td>
+    <td>Define una tabla.</td>
+</tr>
+<tr>
+    <td>&lt;tr&gt;</td>
+    <td>Define una fila en una tabla.</td>
+</tr>
+<tr>
+    <td>&lt;th&gt;</td>
+    <td>Define una celda de encabezado en una tabla.</td>
+</tr>
+<tr>
+    <td>&lt;td&gt;</td>
+    <td>Define una celda en una tabla.</td>
+</tr>
+<!-- Puedes añadir más filas según necesites -->
+</table>
 `;
